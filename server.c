9dd1933c148788fa82a75e084aed4a4148eedfcd
Milan Varghese Joseph <125987303+MJitsme@users.noreply.github.com>
2023-08-06 20:28:32 +0530

    added function to chat

--- server.c
+++ server.c
@@ -13,3 +13,25 @@
 #define SA struct sockaddr
 #define SAI struct sockaddr_in
 
+//creating function to chat
+void chat(int connfd)
+{
+  char buff[max];
+  int n;
+  while(1)
+  {
+    bzero(buff,max);
+    read(connfd,buff,sizeof(buff));
+    printf("message from client: %s",buff);
+    bzero(buff,max);
+    printf("Enter the message to Client:");
+    scanf("%s",buff);
+    write(connfd,buff,sizeof(buff));
+    if strncmp((buff,"exit",4)==0)
+    {
+      printf("Server exits .... ");
+      break;
+    }
+  }
+}
+
