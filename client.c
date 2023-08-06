cd24990e53adb5c69ed029bf4278c8b9212ffe40
Milan Varghese Joseph <125987303+MJitsme@users.noreply.github.com>
2023-08-06 22:21:17 +0530

    Created client.c and imported libraries

---
+++ client.c
@@ -0,0 +1,14 @@
+//importing libraries
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+
+//defining global variables
+#define MAX 100
+#define PORT 9014
+#define SA struct sockaddr
+#define SAI struct sockaddr_in
+
